6478f675015d2d0fb7120e798d98ff46c7119f94
been04 <67615279+been04@users.noreply.github.com>
2021-03-09 09:47:16 +0900

    1.0.0

---
+++ js/main.js
@@ -0,0 +1,170 @@
+$(function(){
+    //luck
+    $('.luck').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .luck').css( 'display', 'block' );
+    });
+
+    //giga
+    $('.giga').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .giga').css( 'display', 'block' );
+    });
+
+    //trot
+    $('.trot').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .trot').css( 'display', 'block' );
+    });
+
+    //event
+    $('.event').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .event').css( 'display', 'block' );
+    });
+
+    //y_news
+    $('.y_news').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .y_news').css( 'display', 'block' );
+    });
+
+    //cube_c
+    $('.cube_c').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .cube_c').css( 'display', 'block' );
+    });
+
+    //kidari
+    $('.kidari').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .kidari').css( 'display', 'block' );
+    });
+
+    //star
+    $('.star').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .star').css( 'display', 'block' );
+    });
+
+    //willog
+    $('.willog').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .willog').css( 'display', 'block' );
+    });
+
+    //fyn
+    $('.fyn').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .uxui').css( 'display', 'block' );
+        $('#up .fyn').css( 'display', 'block' );
+    });
+
+
+
+
+    //new_m
+    $('.new_m').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .new_m').css( 'display', 'block' );
+    });
+
+    //fast
+    $('.fast').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .fast').css( 'display', 'block' );
+    });
+    
+    //disney
+    $('.disney').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .disney').css( 'display', 'block' );
+    });
+
+    //love
+    $('.love').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .love').css( 'display', 'block' );
+    });
+
+    //g_banner
+    $('.g_banner').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .g_banner').css( 'display', 'block' );
+    });
+
+    //ol_banner
+    $('.ol_banner').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .ol_banner').css( 'display', 'block' );
+    });
+
+    //play
+    $('.play').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .play').css( 'display', 'block' );
+    });
+
+    //sang
+    $('.sang').click(function(){
+        $('#up').css( 'display', 'block' );
+        $('#up .visual').css( 'display', 'block' );
+        $('#up .sang').css( 'display', 'block' );
+    });
+    
+
+
+    
+
+    
+
+
+
+
+
+    
+
+    //x
+    $('.x').click(function(){
+        $('#up').css( 'display', 'none' );
+        $('#up .uxui').css( 'display', 'none' );
+        $('#up .visual').css( 'display', 'none' );
+                
+        $('#up .luck').css( 'display', 'none' );
+        $('#up .giga').css( 'display', 'none' );
+        $('#up .trot').css( 'display', 'none' );
+        $('#up .event').css( 'display', 'none' );
+        $('#up .y_news').css( 'display', 'none' );
+        $('#up .cube_c').css( 'display', 'none' );
+        $('#up .kidari').css( 'display', 'none' );
+        $('#up .star').css( 'display', 'none' );
+        $('#up .willog').css( 'display', 'none' );
+        $('#up .fyn').css( 'display', 'none' );
+
+        $('#up .new_m').css( 'display', 'none' );
+        $('#up .fast').css( 'display', 'none' );
+        $('#up .disney').css( 'display', 'none' );
+        $('#up .love').css( 'display', 'none' );
+        $('#up .g_banner').css( 'display', 'none' );
+        $('#up .ol_banner').css( 'display', 'none' );
+        $('#up .play').css( 'display', 'none' );
+        $('#up .sang').css( 'display', 'none' );
+    });
+
+})
